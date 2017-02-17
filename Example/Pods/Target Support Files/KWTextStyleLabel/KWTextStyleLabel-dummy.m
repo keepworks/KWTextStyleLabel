bb393e60f6184d5070081bd5d6828b257b4e2ce1
Pavan Kotesh <kpavankotesh@gmail.com>
2017-02-17 18:45:18 +0530

    Remove Pod files

--- Example/Pods/Target Support Files/KWTextStyleLabel/KWTextStyleLabel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KWTextStyleLabel : NSObject
-@end
-@implementation PodsDummy_KWTextStyleLabel
-@end
--- Example/Pods/Target Support Files/Pods-KWTextStyleLabel_Example/Pods-KWTextStyleLabel_Example-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_KWTextStyleLabel_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KWTextStyleLabel_ExampleVersionString[];
-
